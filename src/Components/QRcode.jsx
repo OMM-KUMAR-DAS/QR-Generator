3142979671c94ffc9d079dff3f00822431bbeaf9
Omm Kumar Das <121632515+OMM-KUMAR-DAS@users.noreply.github.com>
2024-05-30 12:46:41 +0530

    Update QRcode.jsx

--- src/Components/QRcode.jsx
+++ src/Components/QRcode.jsx
@@ -1,39 +1,49 @@
 import React from 'react'
 import { useState } from 'react'
-import {Typography,Box,Grid,TextField} from '@mui/material'
+import {Typography,Box,Button,TextField} from '@mui/material'
 
 const QRcode = () => {
+    
 
     const[inp,setI]= useState("")
     const[data,setQ]= useState()
+    const[load,setload]= useState(false)
+
     
     const omm=async(event)=>{
         event.preventDefault()
         try{
             const res=await fetch(`https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${inp}`)
             setQ(res.url)
+            setload(true)
+            setI("")
         }
         catch(error)
         {
             alert("sorry")
         }
     }
+
   return (
-    <div className='con'>
+
+
+   
+    <Box sx={{backgroundColor:'#14080e',margin:'0',padding:'0px',width:'100%',minHeight:'100vh'}}>
         
 
-        <br></br>
+        
             
-            <form onSubmit={omm}>
-               <Typography varient="h2" style={{ color: '#fff' }} align="center"  fontWeight="bold"  gutterBottom >QR CODE GENERATOR</Typography>
+            <form>
+
+               <Typography varient="h2" sx={{ color: '#fff',textAlign:'center',fontWeight:"bold",width:'100%'}}  >QR CODE GENERATOR</Typography>
                
-               <Box sx={{display:'flex',justifyContent:'center',alignContent:'center',width:'70%',marginLeft:'auto',marginRight:'auto',boxShadow: '0px 0px 25px rgba(0, 0, 0, 0.5)',padding:'10px',marginTop:'40px',borderRadius:'10%'}}>
+               <Box sx={{ width: { xs: '85%', sm: '60%', md: '50%' },display:'flex',justifyContent:'center',alignContent:'center',marginLeft:'auto',marginRight:'auto',boxShadow: '0px 0px 25px rgba(0, 0, 0, 0.5)',padding:'10px',marginTop:'40px',borderRadius:'25px',flexDirection:'column',border:'3px solid white',backgroundColor:'#f7ebe8',boxShadow: "0 0 20px 0 rgba(255, 255, 255, 0.9)"}}>
                
 
-               <Grid spacing={1}>
+             
 
-                    <Grid item xs={12} sm={6} >
                             <TextField
+
                                     required
                                     fullWidth
                                     sx={{ marginBottom: '20px',marginTop:'20px' ,textAlign:'center' }}
@@ -46,29 +56,32 @@ const QRcode = () => {
                                     variant="outlined"
                                     onChange={(event)=>setI(event.target.value)}
                                     />
-                    </Grid> 
+                   
 
 
-                     <Grid item xs={12} sm={6} >
-                            <img className="qrr" src={data} alt=" "/>
-                     </Grid> 
+                     
+                     { 
+                        load &&
+
+                            <img className="qrr" src={data} alt=" " style={{width:'35%',marginLeft:'auto',marginRight:'auto'}}/>
+                     }
+                     
                       <br></br>
                       <br></br>
-                     <Grid item xs={12} sm={6} >
-                          <button className="bt">Generate Qr code</button>
-                     </Grid>   
+                     
+                    <Button onClick={omm} sx={{width:'50%',marginLeft:'auto',marginRight:'auto',border:'2px solid lightblue',borderRadius:'25px'}} varient="outlined"> Generate</Button>
+                   
 
 
 
 
 
-               </Grid>
 
             </Box>    
                 
             </form>
 
-    </div>
+    </Box>
   )
 }
 
